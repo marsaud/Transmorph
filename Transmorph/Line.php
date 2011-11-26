1015133ae5f6c6713fc636146eb638b03527c989
Fabrice Marsaud <marsaud.fabrice@neuf.fr>
2011-11-26 21:54:02 +0100

    added license and started reworking doc tags

--- Transmorph/Line.php
+++ Transmorph/Line.php
@@ -1,7 +1,27 @@
 <?php
 
 /**
+ * This file is part of Transmorph.
+ *
+ * Transmorph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Transmorph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Transmorph. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Fabrice Marsaud <marsaud.fabrice@neuf.fr>
+ * 
  * @package Transmorph
+ * 
+ * @subpackage Line
+ * 
  */
 
 /**
@@ -9,9 +29,8 @@
  *
  * Assure le parsing d'une ligne de mapping (LINE) et encapsule le résultat obtenu.
  * 
- * @author fabrice
- * 
  * @package Transmorph
+ * 
  * @subpackage Line
  * 
  * @property-read string $source ENTRY de lecture de la ligne.
--- Transmorph/Plugin/IteratorNode.php
+++ Transmorph/Plugin/IteratorNode.php
@@ -1,14 +1,36 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This file is part of Transmorph.
+ *
+ * Transmorph is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Transmorph is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Transmorph. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Fabrice Marsaud <marsaud.fabrice@neuf.fr>
+ * 
+ * @package Transmorph
+ * 
+ * @subpackage Plugin
+ * 
  */
 
 /**
  * Description of Transmorph_Plugin_IteratorNode
- *
- * @author root
+ * 
+ * @package Transmorph
+ * 
+ * @subpackage Plugin
+ * 
  */
 class Transmorph_Plugin_IteratorNode extends Transmorph_Plugin_Abstract
 {
